e3e4c7d51b9d300301e699fe581bf92180f40f70
bkrug <benjaminkrug@yahoo.com>
2025-02-01 14:42:21 -0500

    Create collision boxes

---
+++ src/parallaxScroll/COLLISION.asm
@@ -0,0 +1,19 @@
+       DEF  col_detect
+
+box_top     DATA 2
+box_height  DATA 4
+box_width   DATA 6
+
+pig_box     DATA 5*pixel_size,0
+            DATA 16*pixel_size,16*pixel_size
+turtle_box  DATA 0,0
+            DATA 7*pixel_size,15*pixel_size
+rabbit_box  DATA 0,0
+            DATA 15*pixel_size,13*pixel_size
+
+player_box  DATA 3*pixel_size,0
+            DATA 32*pixel_size,7*pixel_size
+
+col_detect
+       
+       RT
